7e6fe73669631de09b429797a72cb52b3a8388a1
Danesh Naik <iamdaneshnaik@gmail.com>
2023-07-02 16:08:51 +0530

    Create AMx minus min-339.java

---
+++ AMx minus min-339.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    
+		    int res=Math.max(a,Math.max(c,b))-Math.min(a,Math.min(c,b));
+		    System.out.println(res);
+		}
+	}
+}
