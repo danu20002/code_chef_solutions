4c9aa6c0ad63a4b87415faa55b971bd0a60cc438
Danesh Naik <iamdaneshnaik@gmail.com>
2023-07-24 22:23:10 +0530

    Create Chef and Wire Frame.java

---
+++ Chef and Wire Frame.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int m=sc.nextInt();
+		    int k=sc.nextInt();
+		    int res=((2*(n+m))*k);
+		    System.out.println(res);
+		}
+	}
+}
