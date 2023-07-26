4344aa5b4769282ae13d1a57ff546ad03cc15600
Danesh Naik <iamdaneshnaik@gmail.com>
2023-07-26 23:17:15 +0530

    Create The gift.java

---
+++ The gift.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan=new Scanner(System.in);
+		int x=scan.nextInt();
+		int n=scan.nextInt();
+		int m=scan.nextInt();
+		if((x+m)>=n)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+		
+	}
+}
