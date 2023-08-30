d423b48f5ef3ff1030acdd6e5b273d52ce892e6f
Danesh Naik <iamdaneshnaik@gmail.com>
2023-08-30 21:18:04 +0530

    Create All New CodeChef.java

---
+++ August long 2023/All New CodeChef.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner  sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(x<y){
+		    System.out.println("OLD");
+		}else if(x==y){
+		    System.out.println("same");
+		}else{
+		    System.out.println("NEW");
+		}
+	}
+}
