87ec245a3def7f19a27820289b828d4fd4443623
Danesh Naik <iamdaneshnaik@gmail.com>
2023-09-01 08:21:50 +0530

    Create how much Scholership.java

---
+++ how much Scholership.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int R=sc.nextInt();
+		if(R<=50){
+		    System.out.println(100);
+		}else if(R>50 && R<=100){
+		    System.out.println(50);
+		    
+		}else{
+		    System.out.println(0);
+		}
+	}
+}
